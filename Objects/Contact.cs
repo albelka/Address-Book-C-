74398067a65bb94123b48bb9b4f3bbfff53f6163
Anne Belka <antastic461@gmail.com>
2016-11-18 08:08:42 -0800

    add index page add get id functions to contacts

--- Objects/Contact.cs
+++ Objects/Contact.cs
@@ -1,35 +1,61 @@
 using System.Collections.Generic;
 
-namespace Contact.Objects
+namespace Contacts.Objects
 {
-  public class Task
+  public class Contact
   {
-    private string _description;
-    private static List<string> _instances = new List<string> {};
+    private string _name;
+    private string _phone;
+    private string _address;
+    private static List<Contact> _instances = new List<Contact> {};
 
-    public Task (string description)
+    public Contact (string name, string phone, string address)
     {
-      _description = description;
+      _name = name;
+      _phone = phone;
+      _address = address;
+      _instances.Add(this);
+      _id = _instances.Count;
     }
-    public string GetDescription()
+    public string GetName()
     {
-      return _description;
+      return _name;
     }
-    public void SetDescription(string newDescription)
+    public void SetName(string newName)
     {
-      _description = newDescription;
+      _name = newName;
     }
-    public static List<string>GetAll()
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+    public in GetId()
+    {
+      return _id;
+    }
+    public static List<Contact>GetAll()
     {
       return _instances;
-    }
-    public void Save()
-    {
-      _instances.Add(_description);
     }
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+    public static Contact Find(int searchId)
+    {
+      return _instances[searcId-1];
     }
   }
 }
--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,7 @@ using Nancy;
 using Nancy.Owin;
 using Nancy.ViewEngines.Razor;
 
-namespace Contact
+namespace Contacts
 {
   public class Startup
   {
